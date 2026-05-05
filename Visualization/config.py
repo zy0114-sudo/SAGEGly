729a58be819741ac5fbe9fa3c0d1f187986d62ae
Yan Zheng <zy0114@sjtu.edu.cn>
2026-05-05 23:26:26 +0800

    Add files via upload

---
+++ Visualization/config.py
@@ -0,0 +1,12 @@
+config_runtime = {
+    '利用正负样本权重':'0.1:0.5',
+    'batch_Size':2,
+    'dragon': 'dragon9',
+    'device': 'cuda:2',
+    'model':'SACEConv',
+    'num_epochs': 600,
+    'jincheng':3,
+    'dropout':0.5,
+    'model':'512-1024',
+    
+}#
